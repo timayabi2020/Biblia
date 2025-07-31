b8d47c96d8b07e6823685aeb1f75c5b31dd34166
Tim <timwamalwa@gmail.com>
2025-08-01 01:23:06 +0300

    Allowed all origins

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            # or ["*"] to allow all origins (use with caution!)
+    allow_origins=["*"],            # or ["*"] to allow all origins (use with caution!)
     allow_credentials=True,
     allow_methods=["*"],              # GET, POST, PUT, DELETE, etc.
     allow_headers=["*"],              # Authorization, Content-Type, etc.
